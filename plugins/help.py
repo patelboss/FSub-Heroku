ba2459a234cc4abd647bfd23a9ea77806a1ec8b0
patelboss <83952959+patelboss@users.noreply.github.com>
2022-03-13 21:29:19 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -44,7 +44,7 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = "https://github.com/DamienSoukara/FSub-Heroku"
         button = [
-            [InlineKeyboardButton(text = '🗣 Support Chat', url="https://t.me/damienhelp")],
+            [InlineKeyboardButton(text = '🗣 Support Chat', url="https://t.me/Filmykeedha")],
             [InlineKeyboardButton(text = '🤖 Source Code', url=url)],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
